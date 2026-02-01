92d54a8b4fdd0d9bf7732538ec65475f08f87735
Thomas Wood <odell.wood@gmail.com>
2026-02-01 15:38:13 -0500

    updating

--- src/crow/agent/__init__.py
+++ src/crow/agent/__init__.py
@@ -1,13 +1,8 @@
 """Crow agent implementations."""
 
-# ACP server has been moved to crow.acp.server
-from crow.acp.server import CrowAcpAgent, sync_main
-
 from .config import LLMConfig, ServerConfig
 
 __all__ = [
     "LLMConfig",
     "ServerConfig",
-    "CrowAcpAgent",
-    "sync_main",
 ]
--- src/crow/agent/__main__.py
+++
@@ -1,6 +0,0 @@
-"""Entry point for running crow ACP server as a module."""
-
-from crow.acp.server import sync_main
-
-if __name__ == "__main__":
-    sync_main()
